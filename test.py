a0f3d9161d51ca7aef123df3137df1f44cd95771
Omar <omarsan786@gmail.com>
2025-06-14 21:15:41 -0400

    pushing test code

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-# import google.generativeai as genai
-# from google.generativeai import types
-
 from google import genai
 from google.genai import types
 
@@ -11,20 +8,53 @@ load_dotenv()
 
 testing_url = "https://www.youtube.com/watch?v=BD-rLIz9XUo&t=1059s"
 
-client = genai.Client(api_key="GEMINI_API_KEY")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+print("\n****** RESPONSE 1 *****")
+
+# response = client.models.generate_content(
+#     model="models/gemini-2.5-pro-preview-05-06",
+#     contents=types.Content(
+#         parts=[
+#             types.Part(
+#                 file_data=types.FileData(file_uri=testing_url)
+#             ),
+#             types.Part(text="Please summarize this video in a short paragraph. Who are the two Elliots?")
+            
+#         ]
+#     )
+# )
+
+print("\n****** RESPONSE 2 *****")
 
 response = client.models.generate_content(
-    model="models/gemini-2.5-pro-preview-05-06",
-    content=types.Content(
+    model="models/gemini-2.5-flash-preview-05-20",
+    contents=types.Content(
         parts=[
             types.Part(
                 file_data=types.FileData(file_uri=testing_url)
             ),
-            types.Part(text="Please summarize this video in a short paragraph. Who are the two Elliots?")
+            types.Part(text="Please summarize this video in a short paragraph. Then explain who are the two 'Elliots'?")
             
         ]
     )
 )
+
+
+print("\n****** RESPONSE 3 *****")
+
+# response = client.models.generate_content(
+#     model="models/gemini-2.0-flash",
+#     contents=types.Content(
+#         parts=[
+#             types.Part(
+#                 file_data=types.FileData(file_uri=testing_url)
+#             ),
+#             types.Part(text="Please summarize this video in a short paragraph. Who are the two Elliots?")
+            
+#         ]
+#     )
+# )
 
 
 # response = client.models.generate_content(
@@ -40,6 +70,7 @@ response = client.models.generate_content(
 # )
 
 print(response.text)
+
 
 
 
